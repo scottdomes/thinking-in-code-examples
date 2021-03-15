9fce2b2282c1c6684d45ee90be90262357d4f53f
Scott <scottdomes@gmail.com>
2021-03-15 22:33:45 +0000

    Module 13

---
+++ module-13/example-1.rb
@@ -0,0 +1,6 @@
+# Run this file by copying 'ruby module-13/example-1.rb' into the Console box in the bottom righthand corner and hitting 'Enter'.
+
+puts "Hello! I'm RubyBot! What's your name?"
+# gets.chomp waits for user input, and then removes any extra spacing
+name = gets.chomp
+puts "Hello #{name}, nice to meet you!"
