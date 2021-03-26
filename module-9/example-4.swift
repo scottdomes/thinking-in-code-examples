27c34b20e1fae830fd1e0e85615ae407978c2a9d
Scott <scottdomes@gmail.com>
2021-03-26 20:31:45 +0000

    Typo

--- module-9/example-4.swift
+++ module-9/example-4.swift
@@ -1,7 +1,7 @@
 // Run this file by copying 'swift module-9/example-4.swift' into the Console box in the bottom righthand corner and hitting 'Enter'.
 
 // Write three functions: one that squares a number, one that cubes a number, and another that loops over an array of numbers and prints the square of numbers over 5 and the cubes of numbers less than or equal to 5
-// Hint: the variable numbers in an array of integers, which is expressed with [Int] in Swift
+// Hint: the variable numbers is an array of integers, which is expressed with [Int] in Swift
 // YOUR CODE HERE
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
