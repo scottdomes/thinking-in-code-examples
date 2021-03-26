f52049815ffb77b1317b0449232a2146f1fb8a00
Scott <scottdomes@gmail.com>
2021-03-26 22:25:43 +0000

    Tweaks

--- module-10/example-3.js
+++ module-10/example-3.js
@@ -1,6 +1,17 @@
 // Run this file by copying 'node module-10/example-3.js' into the Console box in the bottom righthand corner and hitting 'Enter'.
 
 // Combine these three variables into a single dictionary
+const me = {
+  name: "Scott",
+  favoriteFood: "Pizza",
+  favoriteMovie: "Lord of the Rings"
+};
+
+console.log("name:", me.name);
+console.log("food:", me.favoriteFood);
+console.log("movie:", me.favoriteMovie);
+
+
 const name = "Scott";
 const favoriteFood = "Pizza";
 const favoriteMovie = "Lord of the Rings";
--- module-11/example-1.rb
+++ module-11/example-1.rb
@@ -1,7 +1,7 @@
 # Run this file by copying 'ruby module-11/example-1.rb' into the Console box in the bottom righthand corner and hitting 'Enter'.
 
 # The following code is simple... in theory. It creates an array of dictionaries that represent users (with usernames and passwords), and then loops over them, lowercasing all usernames and telling us whether the password is too short or not and whether the user is old enough.
-# Unfortunately, the developer who wrote this code was pretty selfish, and didn't bother to make it readable. It's now your job to try to make this code more readable. That will involve renaming variables and splitting methods into smaller pieces.
+# Unfortunately, the developer who wrote this code was pretty selfish, and didn't bother to make it easy to read. It's now your job to try to make this code more readable. That will involve renaming variables and splitting methods into smaller pieces.
 
 a = { n: 'SCOTTdomes', p: "pass", y: 1991 }
 b = { n: 't3hpEnGu1nofd00m', p: "mysecretpassword", y: 2010 }
