2e16cced1e759b126623a6d2c07e195486ad80c5
Scott <>
2021-04-08 23:16:25 +0000

    Renaming

--- module-6/example-1.py
+++ module-6/example-1.py
@@ -1,10 +1,3 @@
 # Run this file by copying 'python module-6/example-1.py' into the Console box in the bottom righthand corner and hitting 'Enter'.
 
-def square(number):
-  return number * number
-
-result = square(2)
-print(result)
-
-# Write a function that cubes a number
-# YOUR CODE HERE
+## YOUR CODE HERE
---
+++ module-6/example-1b.py
@@ -0,0 +1,10 @@
+# Run this file by copying 'python module-6/example-1.py' into the Console box in the bottom righthand corner and hitting 'Enter'.
+
+def square(number):
+  return number * number
+
+result = square(2)
+print(result)
+
+# Write a function that cubes a number
+# YOUR CODE HERE
