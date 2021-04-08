83c05a69c45244333c5ad4e7639e28e712865330
Scott <>
2021-04-08 23:17:55 +0000

    Improving example

--- module-6/example-1b.py
+++ module-6/example-1b.py
@@ -1,9 +1,9 @@
-# Run this file by copying 'python module-6/example-1.py' into the Console box in the bottom righthand corner and hitting 'Enter'.
+# Run this file by copying 'python module-6/example-1b.py' into the Console box in the bottom righthand corner and hitting 'Enter'.
 
 def square(number):
-  return number * number
+  return number ** 2
 
-result = square(2)
+result = square(12)
 print(result)
 
 # Write a function that cubes a number
